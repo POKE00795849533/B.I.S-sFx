ddb66ad8bf183d36209e3c4068bceaba033a8df4
Dev Fx Hacker Programmer <fx@Jett.com.br>
2025-06-14 22:39:39 -0300

    aprendendo a subir pro git

---
+++ js/home.js
@@ -0,0 +1,22 @@
+const buttonFriends = document.querySelector(".friends")
+const buttonEnimeis = document.querySelector(".enemies")
+const buttonFamily = document.querySelector(".family")
+const buttonColleagues = document.querySelector(".colleagues")
+const buttonPeople = document.querySelector(".people")
+
+
+function openFriends(){
+    window.location.href = "página de amigos";
+}
+function openEnemies(){
+    window.location.href = "página de Inimigos";
+}
+function openFamily(){
+    window.location.href = "página de familia";
+}
+function openColleagues(){
+    window.location.href = "página de colegas";
+}
+function openPeople(){
+    window.location.href = "página de pessoas";
+}
